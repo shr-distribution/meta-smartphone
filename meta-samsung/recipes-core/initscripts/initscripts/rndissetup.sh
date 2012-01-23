9395f28a66c3a54f66ac8a243f7f557ccc7d82c2
Simon Busch <morphis@gravedo.de>
2012-01-23 07:06:24 +0100

    meta-samsung: initscripts: add support for usb gadget in 3.x based kernels and bump PR
    Signed-off-by: Simon Busch <morphis@gravedo.de>

--- meta-samsung/recipes-core/initscripts/initscripts/rndissetup.sh
+++ meta-samsung/recipes-core/initscripts/initscripts/rndissetup.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-echo 1 > /sys/class/usb_composite/rndis/enable
+
+if [ `uname -a | grep 2.6.35` ] ; then
+	# for 2.6.35 based kernels
+        echo 1 > /sys/class/usb_composite/rndis/enable
+else
+        # for 3.0.x based kernels
+        echo 0 > /sys/class/android_usb/android0/enable
+        echo 1 > /sys/class/android_usb/android0/f_rndis/wceis
+        echo 18d1 > /sys/class/android_usb/android0/idVendor
+        echo 4e23 > /sys/class/android_usb/android0/idProduct
+        echo rndis > /sys/class/android_usb/android0/functions
+        echo 224 > /sys/class/android_usb/android0/bDeviceClass
+        echo 1 > /sys/class/android_usb/android0/enable
+fi
