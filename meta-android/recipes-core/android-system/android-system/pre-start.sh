3d2586e2a97c7ed271065abf845a6beb66f0085d
Simon Busch <morphis@gravedo.de>
2014-10-09 20:59:40 -0700

    meta-android: android-system[-image]: don't place ramdisk image in /boot
    On some devices we have to mount the real boot partition on /boot so we can put anything
    else in there. Therefor we place the ramdisk image in /usr/lib/android-system
    
    This also changes the recipe to fail the build when the system image doesn't contain any
    ramdisk image.
    
    Signed-off-by: Simon Busch <morphis@gravedo.de>

--- meta-android/recipes-core/android-system/android-system/pre-start.sh
+++ meta-android/recipes-core/android-system/android-system/pre-start.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-ramdisk=/boot/android-ramdisk.img
+ramdisk=/usr/lib/android-system/ramdisk.img
 
 if [ ! -e $ramdisk ] ; then
 	exit 1
