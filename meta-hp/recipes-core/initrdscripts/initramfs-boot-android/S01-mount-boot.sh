03a6a433d2b714ee4fdd73732d1f7a29128dad8b
Christophe Chapuis <chris.chapuis@gmail.com>
2018-03-08 17:15:30 +0000

    meta-hp: migrate tenderloin to use Halium's init
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

---
+++ meta-hp/recipes-core/initrdscripts/initramfs-boot-android/S01-mount-boot.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# mount boot partition:
+mkdir -p /mnt/boot
+mount /dev/mmcblk0p13 /mnt/boot
+
+# use lvm.static binary file to initialize /dev/store
+[ ! -d /var/lock ] && mkdir -p /var/lock
+[ ! -d /run/lock ] && mkdir -p /run/lock
+
+export LVM_SYSTEM_DIR=/mnt/boot/etc/lvm
+/mnt/boot/usr/sbin/lvm.static vgchange -ay
+
+if [ ! -d /dev/store ]; then
+   panic "Failed to start LVM"
+fi
+
+# initialize the "syspart" variable to the correct path
+syspart=/dev/store/ext3fs
+if [ -e /dev/store/${distro_name}-root ]; then
+    syspart=/dev/store/${distro_name}-root
+fi
+
+if [ ! -e $syspart ]; then
+    panic "Failed to find root partition !"
+fi
+
+# create a file corresponding to Halium's naming criteria for userdata partition
+ln -s media /dev/store/userdata
+
+# override process_bind_mounts(), because in our case userdata is vfat
+# So the symlinks won't be allowed in there
+process_bind_mounts() {
+    tell_kmsg "userdata is vfat: override process_bind_mounts"
+    datadir=${rootmnt}/android/userdata/$distro_name-data
+    mkdir -p $datadir/userdata/.cryptofs
+}
