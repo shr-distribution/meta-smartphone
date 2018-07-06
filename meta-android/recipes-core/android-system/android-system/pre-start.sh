af36b1170e1f8619475db6353be5450f7ca98d32
Christophe Chapuis <chris.chapuis@gmail.com>
2018-07-06 18:44:17 +0000

    android-system: use pre-start.sh from Halium

--- meta-android/recipes-core/android-system/android-system/pre-start.sh
+++ meta-android/recipes-core/android-system/android-system/pre-start.sh
@@ -1,61 +1,32 @@
 #!/bin/sh
 
-# (c) 2013 Canonical LTD.
-# (c) 2013 Simon Busch <morphis@gravedo.de>
-#
-# This program is free software: you can redistribute it and/or modify it
-# under the terms of the the GNU General Public License version 3, as
-# published by the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranties of
-# MERCHANTABILITY, SATISFACTORY QUALITY or FITNESS FOR A PARTICULAR
-# PURPOSE.  See the applicable version of the GNU Lesser General Public
-# License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+for mountpoint in /android/*; do
+	mount_name=$(basename $mountpoint)
+	desired_mount=$LXC_ROOTFS_PATH/$mount_name
 
-# /dev/pts needs to be there
+	# Remove symlinks, for example bullhead has /vendor -> /system/vendor
+	[ -L $desired_mount ] && rm $desired_mount
+
+	[ -d $desired_mount ] || mkdir $desired_mount
+	mount --bind $mountpoint $desired_mount
+done
+
+mknod -m 666 $LXC_ROOTFS_PATH/dev/null c 1 3
+
+# Create /dev/pts if missing
 mkdir -p $LXC_ROOTFS_PATH/dev/pts
 
-mount -t devtmpfs none $LXC_ROOTFS_PATH/dev
-
-mkdir $LXC_ROOTFS_PATH/sys
-mount -t sysfs none $LXC_ROOTFS_PATH/sys
-
-mount_bind_ro() {
-	if [ ! -d $2 ] ; then
-		mkdir $2
-	fi
-	mount --bind $1 $2
-	mount -o remount,ro,bind $2
-}
-
-mount_bind_ro /system $LXC_ROOTFS_PATH/system
-mount_bind_ro /system/vendor $LXC_ROOTFS_PATH/vendor
-mount_bind_ro /firmware $LXC_ROOTFS_PATH/firmware
-mount_bind_ro /system/etc $LXC_ROOTFS_PATH/etc
-if [ -d /persist ] ; then
-  mount_bind_ro /persist $LXC_ROOTFS_PATH/persist
-fi
-
-# usage existing /data directory, rw
-mount -o bind /data $LXC_ROOTFS_PATH/data
-
-# Avoid being spammed with "missing packages.list; retrying"
-touch $LXC_ROOTFS_PATH/data/system/packages.list
-
-# Process any overrides
-if [ -d /var/lib/lxc/android/overrides ] ; then
-	cp -a /var/lib/lxc/android/overrides/* $LXC_ROOTFS_PATH || true
-fi
-
-# Passing sockets between system and container
-rm -Rf /dev/socket
+# Pass /sockets through
 mkdir -p /dev/socket $LXC_ROOTFS_PATH/socket
 mount -n -o bind,rw /dev/socket $LXC_ROOTFS_PATH/socket
+
+rm $LXC_ROOTFS_PATH/sbin/adbd
+
 sed -i '/on early-init/a \    mkdir /dev/socket\n\    mount none /socket /dev/socket bind' $LXC_ROOTFS_PATH/init.rc
 
-# Process any further scripts
+sed -i "/mount_all /d" $LXC_ROOTFS_PATH/init.*.rc
+sed -i "/swapon_all /d" $LXC_ROOTFS_PATH/init.*.rc
+sed -i "/on nonencrypted/d" $LXC_ROOTFS_PATH/init.rc
+
+# Config snippet scripts
 run-parts /var/lib/lxc/android/pre-start.d || true
