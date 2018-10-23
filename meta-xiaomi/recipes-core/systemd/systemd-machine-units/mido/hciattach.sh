a816f9ef19898d9cafec080cd13564d8e192c9d8
Christophe Chapuis <chris.chapuis@gmail.com>
2018-10-23 18:25:13 +0000

    systemd-machine-units: mido: fix Bluetooth
    Use the SMD driver from kernel instead of UART.
    
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

--- meta-xiaomi/recipes-core/systemd/systemd-machine-units/mido/hciattach.sh
+++ meta-xiaomi/recipes-core/systemd/systemd-machine-units/mido/hciattach.sh
@@ -1,16 +1,29 @@
 #!/bin/sh
 
-# first, get the bluetooth address if possible
-if [ -e /persist/bluetooth/.bdaddr ] ; then
-	bdaddr=$(hexdump -e  '/1 "%X:"' /persist/bluetooth/.bdaddr)
-else
-	bdaddr=""
-fi
+#Maximum number of attempts to enable hcismd to try to get
+# hci0 to come online.  Writing to sysfs too early seems to
+# not work, so we loop.
+MAXTRIES=15
 
-# call hciattach (and strip the last char of bdaddr)
-/usr/bin/hciattach ttyHS99 bcm43xx 4000000 flow sleep ${bdaddr%?}
+#setprop bluetooth.hciattach true
+setprop ro.qualcomm.bt.hci_transport smd
+setprop qcom.bt.dev_power_class 2
+setprop qcom.bt.le_dev_pwr_class 2
 
-sleep 1
+i=1
+while [ ! $i -gt $MAXTRIES ]  ; do
+	echo 1 > /sys/module/hci_smd/parameters/hcismd_set
+	sleep 1
+    if [ -e /sys/class/bluetooth/hci0 ] ; then
+        # found hci0, so unblock bluetooth and exit successfully
+		/usr/sbin/rfkill unblock bluetooth
+		hciconfig hci0 up
+		/usr/sbin/rfkill block bluetooth
+        exit 0
+    fi
+    i=$((i+1))
+done
 
-# unblock bluetooth
-/usr/sbin/rfkill unblock bluetooth
+# must have gotten through all our retries, fail
+exit 1
+
