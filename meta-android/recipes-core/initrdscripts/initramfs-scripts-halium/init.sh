bddb3d1ada6241133afb14cb6f572ed5f74bd3a5
Christophe Chapuis <chris.chapuis@gmail.com>
2019-03-08 21:40:40 +0000

    initramfs-boot-android: split halium and generic version
    * provide initramfs-scripts-android for a generic use
    * provide initramfs-scripts-halium for using halium-boot script
    * specify preference through VIRTUAL-RUNTIME_android-initramfs-scripts
    
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

---
+++ meta-android/recipes-core/initrdscripts/initramfs-scripts-halium/init.sh
@@ -0,0 +1,144 @@
+#!/bin/sh
+
+# machine.conf should provide $system_partition (for panic scenario)
+. /machine.conf
+
+# distro.conf should provide $distro_name
+. /distro.conf
+
+# import Halium script
+. /functions
+. /halium-boot.sh
+
+setup_devtmpfs() {
+    mount -t devtmpfs -o mode=0755,nr_inodes=0 devtmpfs $1/dev
+    # Create additional nodes which devtmpfs does not provide
+    test -c $1/dev/fd || ln -sf /proc/self/fd $1/dev/fd
+    test -c $1/dev/stdin || ln -sf fd/0 $1/dev/stdin
+    test -c $1/dev/stdout || ln -sf fd/1 $1/dev/stdout
+    test -c $1/dev/stderr || ln -sf fd/2 $1/dev/stderr
+    test -c $1/dev/socket || mkdir -m 0755 $1/dev/socket
+}
+
+panic() {
+    tell_kmsg "$distro_name initramfs failed:"
+    tell_kmsg "$1"
+    #tell_kmsg "Waiting for 15 seconds before rebooting"
+    #sleep 15s
+    #reboot
+
+    #system partition is needed for accessing build.prop by android-gadget-setup
+    if [ -e /dev/$system_partition ]; then
+        /sbin/fsck.ext4 -p /dev/$system_partition
+        mkdir -p /system
+        mount -t auto -o rw,noatime,nodiratime,nodelalloc /dev/$system_partition /system
+    fi
+
+    #below are now needed in order to use FunctionFS for ADB, tested to work with 3.4+ kernels
+    mkdir -p /dev/usb-ffs/adb 
+    mount -t functionfs adb /dev/usb-ffs/adb > /dev/kmsg
+    #android-gadget-setup doesn't provide below 2 and without them it won't work, so we provide them here.
+    echo adb > /sys/class/android_usb/android0/f_ffs/aliases
+    echo ffs > /sys/class/android_usb/android0/functions 
+
+    /usr/bin/android-gadget-setup adb
+    /usr/bin/adbd
+}
+
+mount_kernel_modules() {
+	# Avoid overriding kernel modules in LuneOS
+    tell_kmsg "Skip overriding of kernel modules"
+}
+
+process_bind_mounts() {
+    # We need to mount some directories read-write in order to have a working
+    # system so bind mount them from the outside into the rootfs. If we're
+    # doing this the first time we have to remove the old data and copy the
+    # initial data
+    datadir=${rootmnt}/userdata/$distro_name-data
+    tell_kmsg "Preparing $datadir"
+    if [ ! -e ${rootmnt}/.firstboot_done ] ; then
+        for dir in var home ; do
+            rm -rf $datadir/$dir
+            mkdir -p $datadir/$dir
+
+            # Copy initial content to new location outside rootfs
+            cp -ra ${rootmnt}/$dir/* $datadir/$dir
+        done
+
+        mkdir -p $datadir/userdata
+        # Copy initial media to userdata
+        cp -ra ${rootmnt}/media/internal/* $datadir/userdata/
+
+        # setup cryptofs which is not a real cryptofs yet
+        if [ -d $datadir/userdata/.cryptofs ] ; then
+            rm -rf $datadir/userdata/.cryptofs
+        fi
+        mkdir -p $datadir/userdata/.cryptofs
+
+        # We're done with our first boot actions
+        touch ${rootmnt}/.firstboot_done
+    fi
+
+    tell_kmsg "Bind-mount the directories"
+    # bind-mount the directories to their correct place
+    for dir in var home ; do
+        mount -o bind,rw $datadir/$dir ${rootmnt}/$dir
+    done
+}
+
+quiet="n"
+
+mkdir -m 0755 /rfs
+rootmnt=/rfs
+
+mkdir -m 0755 /proc
+mount -t proc proc /proc
+mkdir -m 0755 /sys
+mount -t sysfs sys /sys
+mkdir -p /dev
+
+setup_devtmpfs ""
+
+# Check whether we need to boot recovery
+cat /proc/cmdline | grep skip_initramfs
+if [ $? -eq 1 ] && [ -f /recovery/init ] ; then
+    echo "skip_initramfs not found in cmdline. Booting into recovery." > /dev/kmsg
+
+    # mount --bind trick doesn't seem to work with switch_root, using tmpfs
+    mount -t tmpfs -o size=40M tmpfs ${rootmnt}
+    cp -rf /recovery/* ${rootmnt}/
+    exec switch_root ${rootmnt} /init "$@"
+fi
+
+echo "======= LuneOS/Halium ===========" > /dev/kmsg
+
+# Check wether we need to start adbd for interactive debugging
+cat /proc/cmdline | grep enable_adb
+if [ $? -ne 1 ] ; then
+    panic "Initramfs Debug Mode"
+fi
+
+# Start udev
+. udev-start.sh
+
+# Disable busybox's over-restrictive behavior with cpio extraction
+export EXTRACT_UNSAFE_SYMLINKS=1
+
+# Call Halium's mount script
+mountroot
+
+# Stop udevd, we'll miss a few events while we run init, but we catch up
+. udev-stop.sh
+
+tell_kmsg "Umounting unneeded filesystems"
+umount -l /proc
+umount -l /sys
+
+tell_kmsg "Setup the user data directory"
+# finally setup the user data directory
+mount -o bind,rw $datadir/userdata ${rootmnt}/media/internal
+mount -o bind,rw $datadir/userdata/.cryptofs ${rootmnt}/media/cryptofs
+
+tell_kmsg "Switching to root filesystem"
+exec switch_root ${rootmnt} /sbin/init
