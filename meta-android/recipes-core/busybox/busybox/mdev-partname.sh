fe0aaa5fa4e2bfc3ec3c4f6e4730ac8847bcb7bf
Christophe Chapuis <chris.chapuis@gmail.com>
2019-03-27 19:01:24 +0000

    busybox-mdev: also create "by-name" symlinks
    This is used by Halium as well.

--- meta-android/recipes-core/busybox/busybox/mdev-partname.sh
+++ meta-android/recipes-core/busybox/busybox/mdev-partname.sh
@@ -4,11 +4,13 @@
 case "$ACTION" in
     add|"")
         [ -d /dev/disk/by-partname ] || mkdir -p /dev/disk/by-partname
+        [ -d /dev/disk/by-name ]     || mkdir -p /dev/disk/by-name
         # find PARTNAME in /sys/class/block/$MDEV/uevent
         if [ -e "/sys/class/block/$MDEV/uevent" -a -e "/dev/$MDEV"  ]; then
              source "/sys/class/block/$MDEV/uevent"
              # create the symlink
              [ -e /dev/disk/by-partname/"$PARTNAME" ] || ln -s "../../$MDEV" /dev/disk/by-partname/"$PARTNAME"
+             [ -e /dev/disk/by-name/"$PARTNAME"     ] || ln -s "../../$MDEV" /dev/disk/by-name/"$PARTNAME"
         fi
         ;;
     remove)
