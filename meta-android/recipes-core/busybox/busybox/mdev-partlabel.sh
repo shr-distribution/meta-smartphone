a2747caf997bc2d8aefbbeb2cb14b7d487556acf
Christophe Chapuis <chris.chapuis@gmail.com>
2019-05-03 19:31:02 +0000

    busybox: also create by-label entries with mdev
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

--- meta-android/recipes-core/busybox/busybox/mdev-partlabel.sh
+++ meta-android/recipes-core/busybox/busybox/mdev-partlabel.sh
@@ -4,11 +4,22 @@
 case "$ACTION" in
     add|"")
         [ -d /dev/disk/by-partlabel ] || mkdir -p /dev/disk/by-partlabel
+        [ -d /dev/disk/by-label ]     || mkdir -p /dev/disk/by-label
         # find PARTNAME in /sys/class/block/$MDEV/uevent
         if [ -e "/sys/class/block/$MDEV/uevent" -a -e "/dev/$MDEV"  ]; then
              source "/sys/class/block/$MDEV/uevent"
-             # create the symlink
-             [ -e /dev/disk/by-partlabel/"$PARTNAME" ] || ln -s "../../$MDEV" /dev/disk/by-partlabel/"$PARTNAME"
+             if [ -n "$PARTNAME" ]; then
+                # create the symlink
+                [ -e /dev/disk/by-partlabel/"$PARTNAME" ] || ln -s "../../$MDEV" /dev/disk/by-partlabel/"$PARTNAME"
+              fi
+        fi
+
+        if [ -e "/dev/$MDEV"  ]; then
+            # Also give blkid a chance
+            LABEL=$(blkid "/dev/$MDEV" | sed -n 's/.*LABEL=\"\([^\"]*\)\".*/\1/p')
+            if [ -n "$LABEL" ]; then
+                [ -e /dev/disk/by-label/"$LABEL" ] || ln -s "../../$MDEV" /dev/disk/by-label/"$LABEL"
+            fi
         fi
         ;;
     remove)
@@ -17,6 +28,14 @@ case "$ACTION" in
             source "/sys/class/block/$MDEV/uevent"
             # remove the symlink
             [ -e /dev/disk/by-partlabel/"$PARTNAME" ] && rm /dev/disk/by-partlabel/"$PARTNAME"
+        fi
+
+        if [ -e "/dev/$MDEV"  ]; then
+            # Also give blkid a chance
+            LABEL=$(blkid "/dev/$MDEV" | sed -n 's/.*LABEL=\"\([^\"]*\)\".*/\1/p')
+            if [ -n "$LABEL" ]; then
+                [ -e /dev/disk/by-label/"$LABEL" ] && rm /dev/disk/by-label/"$LABEL"
+            fi
         fi
         ;;
     *)
