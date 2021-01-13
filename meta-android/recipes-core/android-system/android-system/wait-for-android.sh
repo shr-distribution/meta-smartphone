57ae6ed81f6f35eb648d167761b7c49bdd89dd6b
Christophe Chapuis <chris.chapuis@gmail.com>
2021-01-13 17:06:11 +0000

    android-system: some more fixes related to Halium 9.0
    Note: mounting /mnt/vendor/persist might be device specific, TBC
    
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

--- meta-android/recipes-core/android-system/android-system/wait-for-android.sh
+++ meta-android/recipes-core/android-system/android-system/wait-for-android.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 
-if [ ! -d /dev/cpuctl ] && [ -d /sys/fs/cgroup/cpu ] ; then
-	mkdir /dev/cpuctl
-	mount --bind /sys/fs/cgroup/cpu /dev/cpuctl
-fi
-
 echo "Waiting for container to come up ..."
 lxc-wait -n android -s RUNNING
 
-containerpid="$(lxc-info -n android -p | grep PID | sed 's/^PID:.* //')"
 while true; do
-	echo "Checking for boot done flag ..."
-	sleep 1
-	[ -f /proc/$containerpid/root/dev/.android_boot_done ] && break
+        echo "Checking for boot done flag ..."
+        sleep 1
+        [ "$(getprop sys.init_boot_completed)" = "1" ] && break
 done
 
 sleep 1
+
 
