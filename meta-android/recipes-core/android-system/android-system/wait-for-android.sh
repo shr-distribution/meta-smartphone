ef22b5e7813536b155336a12eb256bf35c9f5529
Christophe Chapuis <chris.chapuis@gmail.com>
2021-02-15 21:11:12 +0000

    android-system: wait for livedisplay instead of boot property
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

--- meta-android/recipes-core/android-system/android-system/wait-for-android.sh
+++ meta-android/recipes-core/android-system/android-system/wait-for-android.sh
@@ -4,9 +4,14 @@ echo "Waiting for container to come up ..."
 lxc-wait -n android -s RUNNING
 
 while true; do
-        echo "Checking for boot done flag ..."
-        sleep 1
-        [ "$(getprop sys.init_boot_completed)" = "1" ] && break
+	echo "Checking for Livedisplay service ..."
+	sleep 1
+	livedisplay_status=$(/usr/bin/getprop |grep "init.svc.*livedisplay" |grep -o "\[running\]")
+	if [ "$livedisplay_status" = "[running]" ] ; then
+		echo "Android Livedisplay service running"
+		break
+	fi
+#	[ "$(getprop sys.init_boot_completed)" = "1" ] && break
 done
 
 sleep 1
