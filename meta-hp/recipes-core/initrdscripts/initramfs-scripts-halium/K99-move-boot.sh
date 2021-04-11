8135dd3e698344294b63659c2ea69ca9a1ce7770
Christophe Chapuis <chris.chapuis@gmail.com>
2021-04-11 11:29:05 +0000

    tenderloin initramfs-scripts-halium: fix boot mount
    The android rootfs is now read-only, so the /mnt/boot must be moved
    elsewhere.
    The new location will be /uboot on the luneos root filesystem.
    
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

--- meta-hp/recipes-core/initrdscripts/initramfs-scripts-halium/K99-move-boot.sh
+++ meta-hp/recipes-core/initrdscripts/initramfs-scripts-halium/K99-move-boot.sh
@@ -7,17 +7,6 @@ echo 1 > /sys/devices/system/cpu/cpu1/online
 echo 0 > /sys/module/pm_8x60/modes/cpu0/power_collapse/idle_enabled
 
 # Move /mnt/boot to a location under the new root
-tell_kmsg "rootmount: ${rootmnt}"
-mkdir ${rootmnt}/android/boot
-
-for a in `ls ${rootmnt}/android/boot`
-do
-   tell_kmsg "Contents before mount --move in ${rootmnt}/android/boot: $a"
-done
-
-mount --move /mnt/boot ${rootmnt}/android/boot
-
-for b in `ls ${rootmnt}/android/boot`
-do
-   tell_kmsg "Contents after mount --move in ${rootmnt}/android/boot: $b"
-done
+tell_kmsg "Moving boot mount to: ${rootmnt}/uboot"
+mkdir -p ${rootmnt}/uboot
+mount --move /mnt/boot ${rootmnt}/uboot
