d3e8370cf94d2b6bab4137e4dbb5c01396efa781
Christophe Chapuis <chris.chapuis@gmail.com>
2021-05-07 15:51:02 +0000

    android-system: custom android wait for tenderloin
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

---
+++ meta-hp/recipes-core/android-system/android-system/tenderloin/wait-for-android.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "Waiting for container to come up ..."
+lxc-wait -n android -s RUNNING
+
+while true; do
+	echo "Checking for Livedisplay service ..."
+	sleep 1
+	livedisplay_status=$(/usr/bin/getprop |grep "init.svc.*gralloc-2-0" |grep -o "\[running\]")
+	if [ "$livedisplay_status" = "[running]" ] ; then
+		echo "Android Livedisplay service running"
+		break
+	fi
+#	[ "$(getprop sys.init_boot_completed)" = "1" ] && break
+done
+
+sleep 1
+
+
